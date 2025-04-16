2905449131d271147b4f2e5b2518d81f1d498f7d
ha-phan <thuhaphan.vn@gmail.com>
2025-04-16 14:57:39 +0700

    Update GitHub Actions workflow to show trace on failure

--- e2e/api/login-api.spec.js
+++ e2e/api/login-api.spec.js
@@ -4,7 +4,7 @@ test('Kiểm tra API đăng nhập và lấy thông tin người dùng', async (
   // Gửi request đến API và lấy thông tin người dùng
   const response = await page.request.get('/users/1');
   const userInfo = await response.json(); // Lấy dữ liệu JSON từ phản hồi
-
+  console.log(userInfo)
   // Kiểm tra nếu thông tin người dùng được trả về đúng
   expect(userInfo).toHaveProperty('name', 'Leanne Graham');
   expect(userInfo).toHaveProperty('username', 'Bret');
